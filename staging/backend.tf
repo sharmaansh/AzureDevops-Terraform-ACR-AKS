5d80944a9e268dffdc139402517c8895dfa675c6
Himanshu Sharma <53622254+sharmaansh@users.noreply.github.com>
2025-03-16 13:30:38 +0530

    Create backend.tf

---
+++ staging/backend.tf
@@ -0,0 +1,22 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" # Optional but recommended in production
+    }  
+  }
+   backend "azurerm" {
+    resource_group_name  = "terrastate"
+    storage_account_name = "terrastatesa"
+    container_name      = "tfstate"
+    key                 = "staging.terraform.tfstate"
+  } 
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "################"
+  features {}
+}
